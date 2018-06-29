cc0c0d80e6780308ffd05de9bacaddfaceb01798
jhars <jonharlan.io@gmail.com>
2018-06-28 20:00:42 -0400

    Starter Kit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
-class App extends Component {
 
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCCThgSP0AsAQSNAuQbLGyS3iOSCrbigrE',
+      authDomain: 'manager-ee97e.firebaseapp.com',
+      databaseURL: 'https://manager-ee97e.firebaseio.com',
+      projectId: 'manager-ee97e',
+      storageBucket: 'manager-ee97e.appspot.com',
+      messagingSenderId: '800392028030'
+    };
+    firebase.initializeApp(config);
+  }
 
   // LifeCycle Method #1
   render() {
